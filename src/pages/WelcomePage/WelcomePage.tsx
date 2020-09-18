1eb24a14f5aaf93a7a125cfb15973b6d6083828c
Eugene Sokolov <learnmore@MacBook-Pro.local>
2020-09-18 17:52:32 +0300

    front-end: creater route /welcome as protected

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -2,12 +2,18 @@ import * as React from "react";
 import { Component } from "react";
 import { Link } from "react-router-dom";
 import { composeStyles } from "../../helpers/functionHelpers";
+import { RouteComponentProps } from "react-router-dom";
 import styles from "./WelcomePage.module.css";
 import sprite from "../../svgSprite.svg";
 
-class WelcomePage extends Component {
+interface WelcomePageProps extends RouteComponentProps {}
+
+class WelcomePage extends Component<WelcomePageProps> {
   componentDidMount() {
     try {
+      if (localStorage.getItem("wasHere")) {
+        this.props.history.replace("/");
+      }
       localStorage.setItem("wasHere", JSON.stringify(true));
     } catch (err) {
       console.warn(err);
@@ -16,15 +22,15 @@ class WelcomePage extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <div className={styles.poster}>
-          <svg className={styles.svg}>
-            <use href={`${sprite}#welcome_image`}></use>
-          </svg>
-        </div>
+        <svg className={styles.svg}>
+          <use href={`${sprite}#welcome_image`}></use>
+        </svg>
 
         <div className={styles.textContent}>
           <h2 className={styles.title}>Let us save your money</h2>
-          <p className={styles.text}>Welcome everyone who want to control money spending and incomes</p>
+          <p className={styles.text}>
+            Welcome everyone who want to control money spending and incomes
+          </p>
         </div>
 
         <div>
